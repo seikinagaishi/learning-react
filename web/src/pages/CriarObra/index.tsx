2afbe3ede7e7107c2e1ffc7533a45e818b1985ca
Seiki Nagaishi <seiki_nagaishi@hotmail.com>
2021-05-12 15:02:05 -0300

    Página de registro de obra

--- web/src/pages/CriarObra/index.tsx
+++ web/src/pages/CriarObra/index.tsx
@@ -1,44 +1,85 @@
-import React from 'react';
+import React, { useState, FormEvent, ChangeEvent } from 'react';
 import './style.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
 const CriarObra = () => {
+    const history = useHistory();
+
+    const [formData, setFormData] = useState({
+        nome: '',
+        autor: '',
+        ano: '',
+        capitulos: '',
+        foto: 'obra_1.jpg',
+    })
+
+    function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
+    async function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+
+        const {
+            nome,
+            autor,
+            ano,
+            capitulos,
+            foto
+        } = formData;
+
+        const data = {
+            nome,
+            autor,
+            ano,
+            capitulos,
+            foto
+        }
+
+        await api.post('criar', data);
+        history.push('/');
+    }
+
     return(
         <div id="page-criar">
             <main id="main-criar">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h1>CADASTRO DE OBRA</h1>
 
                     <div className="field">
                         <label className="form-label" htmlFor="nome">Nome</label>
-                        <input className="form-input" type="text" name="nome"/>
+                        <input className="form-input" type="text" name="nome" onChange={handleInputChange} required/>
                     </div>
 
                     <div className="field">
                         <label className="form-label" htmlFor="autor">Autor</label>
-                        <input className="form-input" type="text" name="autor"/>
+                        <input className="form-input" type="text" name="autor" onChange={handleInputChange} required/>
                     </div>
 
                     <div className="field-group">
                         <label className="form-label" htmlFor="ano">Ano de Publicação</label>
-                        <input className="form-input" type="number" name="ano"/>
+                        <input className="form-input" type="number" name="ano" onChange={handleInputChange} required/>
                     </div>
 
                     <div className="field-group">
-                        <label className="form-label" htmlFor="eps">Episódios/Capítulos</label>
-                        <input className="form-input" type="text" name="eps"/>
+                        <label className="form-label" htmlFor="capitulos">Episódios/Capítulos</label>
+                        <input className="form-input" type="number" name="capitulos" onChange={handleInputChange} required/>
                     </div>
 
                     <div className="field-file">
-                        <input type="file" name="foto"/>
+                        <input id="file-pic" type="file" name="foto"/>
                         <div className="field-file-image">
                             <p>+</p>
                         </div>
                         <label htmlFor="foto">+ Adicionar foto</label>
                     </div>
 
-                    <button type="button">Cadastrar</button>
+                    <button className="form-button" type="submit">Cadastrar</button>
                 </form>
+
+                <Link to="/" className="btn-link">Cancelar</Link>
             </main>
         </div>
     );
