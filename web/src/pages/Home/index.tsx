bcddd26060fb609ce104d220a97c283b61abe098
Seiki Nagaishi <seiki_nagaishi@hotmail.com>
2021-05-12 15:01:46 -0300

    Página Home

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,16 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import icon from '../../assets/pencil.png';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
+interface obraArray {
+    id: number,
+    nome: string,
+    autor: string,
+    ano: number,
+    capitulos: number,
+    foto_url: string
+}
 
 const Home = () => {
+    const history = useHistory();
+
     useEffect(() => {
         const option = document.getElementsByClassName('options');
         for(let i = 0; i < option.length; i++) {
             option[i].addEventListener('click', () => {
                 let dropdown = document.getElementsByTagName('ul')[i];
-                if(dropdown.className == 'dropdown-close') {
+                if(dropdown.className === 'dropdown-close') {
                     let alreadyOpened = document.querySelector('.dropdown');
                     if(alreadyOpened) {
                         alreadyOpened.className = 'dropdown-close';
@@ -24,47 +35,70 @@ const Home = () => {
         }
     });
 
+    const [obras, setObras] = useState<obraArray[]>([]);
+
+    useEffect(() => {
+        api.get('lista').then(res => {
+            setObras(res.data);
+        })
+    }, []);
+
+    useEffect(() => {
+        const btnsExcluir = document.getElementsByClassName('btn-excluir');
+        for(let i = 0; i < btnsExcluir.length; i++) {
+            btnsExcluir[i].addEventListener('click', () => {
+                var id = btnsExcluir[i].id
+
+                api.delete(`deletar/${id}`);
+                history.go(0);
+            });
+        }
+    })
+    
+
     return(
         <div id="page-home">
             <main id="home">
-                <section className="card">
-                    <div className="card-header">
-                        <h1>Formação Rochosa</h1>
-                        <button type="button" className="options">•••</button>
-                        <ul className="dropdown-close">
-                            <li>Excluir obra</li>
-                        </ul>
-                    </div>
+                {obras.map((obra) => (
+                    <section className="card" key={obra.id}>
+                        <div className="card-header">
+                            <h1>{obra.nome}</h1>
+                            <button type="button" className="options">•••</button>
+                            <ul className="dropdown-close">
+                                <li id={'' + obra.id} className="btn-excluir">Excluir obra</li>
+                            </ul>
+                        </div>
 
-                    <div className="card-body">
-                        <section className="card-left">
-                            <div className="card-image">
-                                <img src="" alt="Obra" />
-                            </div>
-                        </section>
-
-                        <aside>
-                            <section className="card-info">
-                                <div className="info">
-                                    <p className="info-title">AUTOR</p>
-                                    <p className="info-description">Ian Beckley</p>
-                                </div>
-                                <div className="info-group">
-                                    <p className="info-title">ANO</p>
-                                    <p className="info-description">2019</p>
-                                </div>
-                                <div className="info-group">
-                                    <p className="info-title">EPS/CAPS</p>
-                                    <p className="info-description">0</p>
+                        <div className="card-body">
+                            <section className="card-left">
+                                <div className="card-image">
+                                    <img src={obra.foto_url} alt={obra.nome} />
                                 </div>
                             </section>
-                        </aside>
-                    </div>
-                </section>
+
+                            <aside>
+                                <section className="card-info">
+                                    <div className="info">
+                                        <p className="info-title">AUTOR</p>
+                                        <p className="info-description">{obra.autor}</p>
+                                    </div>
+                                    <div className="info-group">
+                                        <p className="info-title">ANO</p>
+                                        <p className="info-description">{obra.ano}</p>
+                                    </div>
+                                    <div className="info-group">
+                                        <p className="info-title">EPS/CAPS</p>
+                                        <p className="info-description">{obra.capitulos}</p>
+                                    </div>
+                                </section>
+                            </aside>
+                        </div>
+                    </section>
+                ))};
             </main>
 
             <Link className="btn-criar" to="/criar">
-                <img src={icon} />
+                <img alt="Criar Postagem" src={icon} />
             </Link>
         </div>
     );
